9c341bc56a49dd53ca7362133c92949cc2f77fc0
Syeda Aleeza Abbas <166072392+syedaaleezaabbas@users.noreply.github.com>
2024-04-12 20:11:25 +0500

    push my todo list to github

---
+++ main.ts
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let condition = true;
+
+while (condition) {
+    let ans = await inquirer.prompt([
+        {
+            name: "select",
+            type: "list",
+            message: "Select an operation",
+            choices: ["Add", "update", "view", "delete", "exit"],
+        }
+    ]);
+
+ if (ans.select === "Add"){
+    let addTodo = await inquirer.prompt({
+        name: "todo",
+        type: "input",
+        message: "Add items in the list",
+    })
+    todos.push(addTodo.todo);
+    console.log(todos);
+ }    
+
+ if (ans.select === "update"){
+    let updateTodo = await inquirer.prompt({
+        name: "todo",
+        type: "list",
+        message: "Update items in the list",
+        choices: todos.map(item => item)
+    })
+    let addTodo = await inquirer.prompt({
+        name: "todo",
+        type: "input",
+        message: "Add items in the list",
+    })
+    let newTodo = todos.filter(val => val !== updateTodo.todo);
+    todos = [...newTodo,addTodo.todo];
+    console.log(todos);
+ }
+
+ if (ans.select === "view"){
+    console.log(todos);
+ }
+
+ if (ans.select === "delete"){
+    let deleteTodo = await inquirer.prompt({
+        name: "todo",
+        type: "list",
+        message: "Select item to delete",
+        choices: todos.map(item => item)
+    })
+    let newTodo = todos.filter(val => val !== deleteTodo.todo);
+    todos = [...newTodo];
+    console.log(todos);
+ } 
+ 
+ if (ans.select === "exit"){
+     console.log("Exiting program...");
+    condition = false;
+ }
+}
+     
